f2153ca7fdf675c1a7d455c9fbbe485ef546d2f3
Henry Lee <henrylee@boohee.com>
2013-05-24 18:10:40 +0800

    [NewFeature] first init reservation system

---
+++ lib/reservation_system.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+
+class ReservationSystem
+
+  class << self
+    def author
+      "Henry Lee"
+    end
+  end
+end
--- test/test_reservation_system.rb
+++ test/test_reservation_system.rb
@@ -1,7 +1,9 @@
+# -*- encoding : utf-8 -*-
+
 require 'helper'
 
 class TestReservationSystem < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_henry
+    assert ReservationSystem.author
   end
 end
